36a9ed2b50237992784fa4817e78506daa42961d
Karol Przybylak <karolprzybylak2@gmail.com>
2023-02-06 16:54:59 +0100

    Scrap cars form link and add it as objects to list

---
+++ AutoScraper.cs
@@ -0,0 +1,93 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WebScraper_01
+{
+    internal class AutoScraper
+    {
+        private const string BaseUrl = "https://www.otomoto.pl/osobowe/ford/fiesta?search%5Bfilter_enum_generation%5D=gen-mk8-2017&search%5Bfilter_float_price%3Ato%5D=30000&search%5Badvanced_search_expanded%5D=true";
+
+        public void GetCars()
+        {
+            var web = new HtmlWeb();
+            var document = web.Load(BaseUrl);
+            List<CarModel> carsModels = new List<CarModel>();
+
+            var CarOffers = document.QuerySelectorAll("main article");
+
+            foreach (var CarOffer in CarOffers)
+            {
+                var name = CarOffer.QuerySelector("h2").InnerText;
+                //Console.WriteLine(CarOffer.QuerySelectorAll("div")[4].InnerText);
+                int price = 3;
+
+                var lists = CarOffer.QuerySelectorAll("ul");
+                var properties = lists[1].QuerySelectorAll("li");
+                var offerInfo = lists[2].QuerySelectorAll("li");
+
+                int year = int.Parse(properties[0].InnerText);
+                int mileage = int.Parse(MileageTrim(properties[1].InnerText));
+                double engineSize = double.Parse(EngineTrim(properties[2].InnerText));
+                var fuelType = properties[3].InnerText;
+
+                var localization = RemoveSpecialChars(TrimCSS(offerInfo[0].InnerText));
+
+                var publicated = offerInfo[1].InnerText;
+                var href = CarOffer.QuerySelector("h2 a").Attributes["href"].Value;
+
+                if (properties[1].InnerText == offerInfo[1].InnerText)
+                {
+                    publicated = "no data";
+                }
+
+                carsModels.Add(new CarModel(name, price, year, mileage, engineSize, fuelType, localization, publicated, href));
+            }
+
+            foreach (var CarOffer in carsModels)
+            {
+                CarOffer.CarInfo();
+            }
+        }
+
+        public string RemoveSpecialChars(string input)
+        {
+            return Regex.Replace(input, @"[<>\(\)\!\?\-_]", string.Empty);
+        }
+
+        public string TrimCSS(string input)
+        {
+            int index = input.IndexOf("}");
+            if (index == -1) return input;
+
+            char nextChar = input[index + 1];
+            if (Char.IsUpper(nextChar))
+            {
+                return input.Substring(index + 1);
+            }
+            else
+            {
+                return TrimCSS(input.Substring(index + 1));
+            }
+        }
+
+        public string MileageTrim(string input)
+        {
+            input = input.Trim();
+            input = Regex.Replace(input, @"[^\d]", string.Empty);
+            return input;
+        }
+
+        public string EngineTrim(string input)
+        {
+            input = Regex.Replace(input, @"[^\d]", string.Empty);
+            double inputDouble = double.Parse(input);
+            inputDouble -= 3;
+            inputDouble = inputDouble/10000;
+            inputDouble = Math.Round(inputDouble, 1);
+            return inputDouble.ToString();
+        }
+    }
+}
---
+++ CarModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebScraper_01
+{
+    internal class CarModel
+    {
+        public CarModel(string name, int price, int yearOfProduction, int mileageKM, double engineSize, string fuelType, string localization, string publicated, string link)
+        {
+            Name = name;
+            Price = price;
+            YearOfProduction = yearOfProduction;
+            MileageKM = mileageKM;
+            EngineSize = engineSize;
+            FuelType = fuelType;
+            Localization = localization;
+            Publicated = publicated;
+            Link = link;
+
+            if (yearOfProduction.ToString() == localization)
+            {
+                Localization = "no data";
+            }
+        }
+
+        public string Name{ get; set; }
+        public int Price { get; set; }
+        public int YearOfProduction { get; set; }
+        public int MileageKM{ get; set; }
+        public double EngineSize{ get; set; }
+        public string FuelType{ get; set; }
+        public string Localization { get; set; }
+        public string Publicated { get; set; }
+        public string Link { get; set; }
+
+        public void CarInfo()
+        {
+            Console.WriteLine($"Nazwa: {Name}");
+            Console.WriteLine($"Cena: {Price} PLN");
+            Console.WriteLine($"Rocznik: {YearOfProduction}");
+            Console.WriteLine($"Przebieg: {MileageKM} km");
+            Console.WriteLine($"Pojemność skokowa: {EngineSize}L");
+            Console.WriteLine($"Paliwo: {FuelType}");
+            Console.WriteLine($"Lokalizacja: {Localization}");
+            Console.WriteLine($"{Publicated}");
+            Console.WriteLine($"link: {Link}");
+
+            Console.WriteLine("------------------");
+        }
+    }
+}
