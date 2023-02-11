382cdffef088b2d749989f7143fc9fbece60ac1a
Karol Przybylak <karolprzybylak2@gmail.com>
2023-02-11 22:29:53 +0100

    Link Generator Added

--- CarScraper.cs
+++ CarScraper.cs
@@ -9,7 +9,8 @@ namespace WebScraper_01
 {
     internal class CarScraper
     {
-        private string BaseUrl = "https://www.otomoto.pl/osobowe/bmw/seria-3?search%5Bfilter_enum_generation%5D=gen-e46-1998-2007&search%5Border%5D=created_at%3Adesc&search%5Bfilter_float_price%3Ato%5D=25000&search%5Badvanced_search_expanded%5D=true";
+        private static string CreateUrl = Utilities.CreateBaseUrl();
+        private string BaseUrl = CreateUrl;
 
 
         public void GetCars(ref List<CarModel> carsModels)
@@ -36,38 +37,46 @@ namespace WebScraper_01
 
                 foreach (var CarOffer in CarOffers)
                 {
-                    var name = CarOffer.QuerySelector("h2").InnerText;
-
-                    var lists = CarOffer.QuerySelectorAll("ul");
-                    var properties = lists[1].QuerySelectorAll("li");
-                    var offerInfo = lists[2].QuerySelectorAll("li");
-
-                    int year = int.Parse(properties[0].InnerText);
-                    int mileage = int.Parse(MileageTrim(properties[1].InnerText));
-                    double engineSize = double.Parse(EngineTrim(properties[2].InnerText));
-                    string fuelType = "";
-
                     try
                     {
-                        fuelType = properties[3].InnerText;
+                        var name = CarOffer.QuerySelector("h2").InnerText;
+
+                        var lists = CarOffer.QuerySelectorAll("ul");
+                        var properties = lists[1].QuerySelectorAll("li");
+                        var offerInfo = lists[2].QuerySelectorAll("li");
+
+                        int year = int.Parse(properties[0].InnerText);
+                        int mileage = int.Parse(MileageTrim(properties[1].InnerText));
+                        double engineSize = double.Parse(EngineTrim(properties[2].InnerText));
+                        string fuelType = "";
+
+                        try
+                        {
+                            fuelType = properties[3].InnerText;
+                        }
+                        catch (Exception)
+                        {
+                            fuelType = "Brak danych";
+                        }
+
+                        var localization = RemoveSpecialChars(TrimCSS(offerInfo[0].InnerText));
+                        var publicated = offerInfo[1].InnerText;
+                        var href = CarOffer.QuerySelector("h2 a").Attributes["href"].Value;
+
+                        int price = 0;
+
+                        if (properties[1].InnerText == offerInfo[1].InnerText)
+                        {
+                            publicated = "no data";
+                        }
+
+                        carsModels.Add(new CarModel(name, price, year, mileage, engineSize, fuelType, localization, publicated, href));
                     }
                     catch (Exception)
                     {
-                        fuelType = "Brak danych";
+                        continue;
                     }
-
-                    var localization = RemoveSpecialChars(TrimCSS(offerInfo[0].InnerText));
-                    var publicated = offerInfo[1].InnerText;
-                    var href = CarOffer.QuerySelector("h2 a").Attributes["href"].Value;
-
-                    int price = 0;
-
-                    if (properties[1].InnerText == offerInfo[1].InnerText)
-                    {
-                        publicated = "no data";
-                    }
-
-                    carsModels.Add(new CarModel(name, price, year, mileage, engineSize, fuelType, localization, publicated, href));
+                    
                 }
                 actualPageNumber++;
                 NextPage(ref document, ref isNextPage, lastPageNumber, ref actualPageNumber);
@@ -91,7 +100,7 @@ namespace WebScraper_01
                     throw new Exception();
                 }
                 var web = new HtmlWeb();
-                string href = BaseUrl + "&page=" + actualPageNumber.ToString();
+                string href = BaseUrl + "?page=" + actualPageNumber.ToString();
                 document = web.Load(href);
             }
             catch (Exception)
---
+++ Utilities.cs
@@ -0,0 +1,144 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebScraper_01
+{
+    internal static class Utilities
+    {
+        public static string CreateBaseUrl()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");
+
+            IWebDriver driver = new ChromeDriver(options);
+
+
+            driver.Navigate().GoToUrl("https://www.otomoto.pl");
+
+            driver.FindElement(By.XPath("//div[contains(@role, 'alertdialog')]//button[contains(@id, 'accept')]")).Click();
+
+            IWebElement submitButton = driver.FindElement(By.XPath("//form//button[@data-testid='submit-btn']"));
+            IWebElement brandButton = driver.FindElement(By.XPath("//form//div[@id='filter_enum_make']//button"));
+            brandButton.Click();
+
+            IWebElement brandUl = driver.FindElement(By.XPath("//form//div[@id='filter_enum_make']//ul"));
+            List<IWebElement> brandLiList = brandUl.FindElements(By.XPath("//form//div[@id='filter_enum_make']//ul/li")).ToList();
+            brandLiList.RemoveRange(0, 2);
+            brandLiList = brandLiList.Where(m => !m.GetAttribute("innerText").Contains("(0)")).ToList();
+
+            int brandNumber = 1;
+            foreach (var carName in brandLiList)
+            {
+                Console.WriteLine($"{brandNumber}. {carName.Text}");
+                brandNumber++;
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write($"Podaj marke (1-{brandLiList.Count}): ");
+                    int brandInput = int.Parse(Console.ReadLine());
+                    brandLiList[brandInput - 1].Click();
+                    break;
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+            IWebElement modelButton = driver.FindElement(By.XPath("//form//div[@id='filter_enum_model']//button"));
+            modelButton.Click();
+
+
+            IWebElement modelUl = driver.FindElement(By.XPath("//form//div[@id='filter_enum_model']//ul"));
+            List<IWebElement> modelLiList = modelUl.FindElements(By.XPath("//form//div[@id='filter_enum_model']//ul/li")).ToList();
+            modelLiList.RemoveRange(0, 1);
+            modelLiList = modelLiList.Where(m => !m.GetAttribute("innerText").Contains("(0)")).ToList();
+
+            int modelNumber = 1;
+            foreach (var carName in modelLiList)
+            {
+                Console.WriteLine($"{modelNumber}. {carName.Text}");
+                modelNumber++;
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write($"Podaj model (1-{modelLiList.Count}): ");
+                    int modelInput = int.Parse(Console.ReadLine());
+                    modelLiList[modelInput - 1].Click();
+                    break;
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+
+            IWebElement generationButton = driver.FindElement(By.XPath("//form//div[@id='filter_enum_generation']//button"));
+            generationButton.Click();
+
+            try
+            {
+                var tryGenerationList = driver.FindElement(By.XPath("//form//div[@id='filter_enum_generation']//ul"));
+            }
+            catch (Exception)
+            {
+                submitButton.Click();
+                Thread.Sleep(1000);
+                string urlNoGeneration = driver.Url;
+                driver.Close();
+
+                return urlNoGeneration;
+            }
+
+            IWebElement generationUl = driver.FindElement(By.XPath("//form//div[@id='filter_enum_generation']//ul"));
+            List<IWebElement> generationLiList = generationUl.FindElements(By.XPath("//form//div[@id='filter_enum_generation']//ul/li")).ToList();
+            generationLiList.RemoveRange(0, 1);
+            generationLiList = generationLiList.Where(m => !m.GetAttribute("innerText").Contains("(0)")).ToList();
+
+            int generationNumber = 1;
+            foreach (var carName in generationLiList)
+            {
+                Console.WriteLine($"{generationNumber}. {carName.Text}");
+                generationNumber++;
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write($"Podaj generacje (1-{generationLiList.Count}): ");
+                    int generationInput = int.Parse(Console.ReadLine());
+                    generationLiList[generationInput - 1].Click();
+                    break;
+                }
+                catch (Exception)
+                {
+                    
+                }
+            }
+
+            submitButton.Click();
+
+            Thread.Sleep(1000);
+            string url = driver.Url;
+            driver.Close();
+
+            return url;
+        }
+
+    }
+}
