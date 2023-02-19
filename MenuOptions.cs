44cdda21c9afb31a59e939fb4fee7c6472a946c8
Karol Przybylak <karolprzybylak2@gmail.com>
2023-02-19 15:15:54 +0100

    Export to json

--- MenuOptions.cs
+++ MenuOptions.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,7 +28,12 @@ namespace WebScraper_01
 
         public static void ExpotToJSON(List<CarModel> cars)
         {
+            var json = JsonConvert.SerializeObject(cars);
 
+            string projectFolderPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string filePath = Path.Combine(projectFolderPath, "raport.json");
+
+            File.WriteAllText(filePath, json);
         }
     }
 }
